5805febf97c5a8d0d6c526c0a712c9b49795c33a
Suprita13 <suprita@digi9.co.in>
2025-05-25 19:47:40 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './Pages/Login/Login';
+// import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
 import BaseLayout from './Pages/BaseLayout/BaseLayout';
 import LeaveManagement from './modules/Admin/Pages/LeaveManagement/LeaveManagement';
