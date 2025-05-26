1e3c6ac7389ff54a5a939eaffba6aad63d6fd163
Roshnipatil31 <Roshni@digi9.co.in>
2025-05-26 10:58:14 +0530

    feat: Add getAllUsers endpoint to retrieve all users

--- server/routes/employeeRoute.js
+++ server/routes/employeeRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { register,AuthUser } from '../controllers/Auth.js';
+import { register,AuthUser, getAllUsers } from '../controllers/Auth.js';
 import { forAdmin,forEmployee ,authenticateToken} from '../middleware/authMiddleware.js';
 
 
@@ -10,3 +10,4 @@ export  const EmployeeRouter = express.Router()
 
 EmployeeRouter.post('/register',authenticateToken,forAdmin,register,);
 EmployeeRouter.post('/login',AuthUser);
+EmployeeRouter.get('/users',getAllUsers);
