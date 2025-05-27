38494c186b0c741fcbef512cabc5ee1e63d5ef40
Suprita13 <suprita@digi9.co.in>
2025-05-27 18:41:42 +0530

    changes

--- server/routes/balanceRoute.js
+++ server/routes/balanceRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllBalanceLeaves } from '../controllers/BalanceLeave.js'; // make sure the path & extension are correct
+import { getAllBalanceLeaves, getBalanceByEmployeeId } from '../controllers/BalanceLeave.js'; // make sure the path & extension are correct
 
 const BalanceRouter = express.Router(); // ✅ use `Router()` not `router`
 
 BalanceRouter.get('/get-all-leaves', getAllBalanceLeaves);
+BalanceRouter.get('/get-balance/:id', getBalanceByEmployeeId);
 
 
 export default BalanceRouter; // ✅ export the router
